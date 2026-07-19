05e2579227132dd30f63a32aabebc537ecaa2969
Magzine <MGMES@proton.me>
2026-07-19 16:33:03 +0700

    Update V2EH_test.js

--- V2EH_test.js
+++ V2EH_test.js
@@ -21,6 +21,15 @@ Deno.test("Removing EventSlot", () => {
   expect(eh.eventData[0]).toBeUndefined();
 });
 
+Deno.test("Remove Slot from EventID", () => {
+  eh.CreateEvent(1, () => { console.log("AF") }, "B4")
+  eh.CreateEvent(1, () => { console.log("AFFF") }, "B4")
+
+  eh.RemoveEventID("B4",1)
+  eh.RemoveEventSlot(4)
+  expect(eh.eventID["B4"]).toMatchObject([0])
+})
+
 let cout = 0;
 Deno.bench(`Creating Event`, (b) => {
   eh.CreateEvent(
@@ -39,12 +48,11 @@ Deno.bench("Triggering Event", () => {
 });
 let indx = 0;
 Deno.bench("Deleting Event", (b) => {
-  b.start();
   eh.RemoveEventID(indx);
   eh.RemoveEventSlot(indx);
-  b.end();
   indx++;
 });
+
 
 Deno.test("Creating 100000 Event", () => {
   for (let i = 0; i <= 100000; i++) {
@@ -68,6 +76,7 @@ Deno.test("Deleting 100000 Event", () => {
     eh.RemoveEventSlot(i);
   }
 });
+
 
 Deno.test("Single Multi Event", () => {
   eh.CreateEvent(
@@ -127,8 +136,7 @@ Deno.test("Triggering CD in CB_test", () => {
 Deno.test("Triggering CB_test", () => {
   eh.Trigger(1, "CB_test");
 });
-
-Deno.test("Grouping JD to CB_test", () => {
+Deno.test("Grouping CD to CB_test", () => {
   eh.CreateEvent(4, () => {}, "CB_test", "JD");
 
   eh.CreateEvent(
@@ -140,5 +148,9 @@ Deno.test("Grouping JD to CB_test", () => {
     "JD",
     "Joe",
   );
-  eh.Trigger(1, "CB_test", "JD", "Joe");
+  eh.Trigger(1, "CB_test", "CD", "Joe");
+});
+
+Deno.test("Triggering CB_test Group", () => {
+  eh.Trigger(1, "CB_test");
 });
